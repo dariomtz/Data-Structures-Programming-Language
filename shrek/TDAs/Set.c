0a18be6f71a1784f6e7bdf71ca9815288c51e832
MAGP116 <44816914+MAGP116@users.noreply.github.com>
2019-12-05 13:43:38 -0600

    implemented all the functions of set
    implemented all that stuff ok.

--- shrek/TDAs/Set.c
+++ shrek/TDAs/Set.c
@@ -6,10 +6,230 @@
 
 #include "Set.h"
 
+typedef struct strNode * node;
 
-/*
-    set must have one tree for integers and floats and another tree for strings
-    IT CANT HAVE OTHER CONTAINERS INSIDE.
-    Use the cmp function in stdhead to compare them
- */
+struct strNode{
+	node left;
+	node right;
+	Data data;
+};
+
+
+struct strSet{
+	int size;
+	Data name;
+	node sets[3]; //int float strings
+};
+
+//Internals Functions
+void print(node n) {
+	if (!n)
+		return;
+	print(n->left);
+	data_print(n->data);
+	if(n->right != NULL)
+		printf(", ");
+	print(n->right);
+};
+
+void destroy(node n){
+	if(!n)
+		return;
+	destroy(n->left);
+	destroy(n->right);
+	data_destroy(n->data);
+	free((void*)n);
+	return;
+};
+
+void addLink(node p, node c, bool isLeftChild) {
+	if (!p)
+		return;
+	if (isLeftChild == TRUE)
+		p->left = c;
+	else
+		p->right = c;
+
+	return;
+};
+
+node almostBigger(node n){
+	node p = NULL;
+	while(n->right){
+		p = n;
+		n = n->right;
+	}
+	return p;
+}
+
+void removeBranch(Set s, node p, bool isLeftChild, int a){
+	if(!s || !s->sets[a])
+		return;
+	node n;
+	n = (!p)?s->sets[a]:(isLeftChild)?p->left:p->right;
+
+	if(!n->left && !n->right){
+		if(!p) // caso 1 con raiz
+			s->sets[a] = NULL;
+		else // caso 1
+			addLink(p,NULL,isLeftChild);
+		free((void*)n);
+		s->size--;
+		return;
+	}
+	if (!n->left || !n->right) {
+		if (!p) {
+			if (n->left) // caso 2a raiz
+				s->sets[a] = n->left;
+			else // caso 2b raiz
+				s->sets[a] = n->right;
+		}
+		else {
+			if (n->left) //caso 2a
+				addLink(p, n->left, isLeftChild);
+			else // caso 2b
+				addLink(p, n->right, isLeftChild);
+		}
+		free((void*) n);
+		s->size--;
+		return;
+	}
+
+	node pm = almostBigger(n->left);
+	Type t;
+	if (pm) {// caso 3b
+		t = (pm->right)->data;
+		removeBranch(s, pm, FALSE);
+		n->data = t;
+	}
+	else {// caso 3a
+		t = (n->left)->data;
+		removeBranch(s,n,TRUE);
+		n->data = t;
+	}
+	return;
+};
+
+//External functions
+
+Set set_create(Data name){
+	Set s = (Set)calloc(1,sizeof(struct strSet));
+	s->name = name;
+	return s;
+};
+Data set_name(Set set){
+	return (!set? NULL:set->name);
+};
+void set_destroy(Set set){
+	if (!set)
+		return;
+	for(int i = 0; i<3;i++)
+		destroy(set->sets[i]);
+	free((void*) set);
+	return;
+}
+;
+int set_size(Set set){
+	return (!set? -1:set->size);
+};
+bool set_add(Set set, Data data){
+	if (!set || !data)
+		return FALSE;
+	node nt = (node) calloc(1, sizeof(struct strNode));
+	nt->data = data;
+	int a =(data->type == INT || data->type == FLOAT)? 0:(data->type == STRING)?1:2;
+	if (!set->sets[a]) {
+		set->sets[a] = nt;
+		set->size++;
+		return TRUE;
+	}
+	node n;
+	int r;
+	n = set->sets[a];
+	while (n) {
+		r = data_cmp(data, n->data);
+		if (r == 0) {
+			free((void*) nt);
+			return FALSE;
+		}
+		if (r > 0) {
+			if (n->right == NULL) {
+				n->right = nt;
+				set->size++;
+				return TRUE;
+			} else {
+				n = n->right;
+			}
+		} else {
+			if (n->left == NULL) {
+				n->left = nt;
+				set->size++;
+				return TRUE;
+			} else {
+				n = n->left;
+			}
+		}
+	}
+	free((void*) nt);
+	return FALSE;
+};
+bool set_contains(Set set, Data data){
+	if (!set || !data)
+		return FALSE;
+	int a =(data->type == INT || data->type == FLOAT)? 0:(data->type == STRING)?1:2;
+	node n = set->sets[a];
+	int r;
+	while (n) {
+		r = data_cmp(data, n->data);
+		if (r == 0)
+			return TRUE;
+		if (r > 0)
+			n = n->right;
+		else
+			n = n->left;
+	}
+	return FALSE;
+};
+bool set_remove(Set set, Data data){
+	if (!set || !data)
+		return FALSE;
+	int a =(data->type == INT || data->type == FLOAT)? 0:(data->type == STRING)?1:2;
+	node n = set->sets[a];
+	node p = NULL;
+	int r;
+	bool isLeftChild;
+	while (n) {
+		r = data_cmp(data, n->data);
+		if (r == 0) {
+			break;
+		}
+		if (r > 0) {
+			p = n;
+			n = n->right;
+			isLeftChild = FALSE;
+		} else {
+			p = n;
+			n = n->left;
+			isLeftChild = TRUE;
+		}
+	}
+	if (!n)
+		return FALSE;
+	removeBranch(set, p, isLeftChild, a);
+	return TRUE;
+};
+void set_print(Set set) {
+	if (!set)
+		return;
+	printf("{");
+	for (int i = 0; i < 3; i++)
+		print(set->sets[i]);
+	printf("}");
+	return;
+}
+;
+
+
+
+
 
